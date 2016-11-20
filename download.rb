89a314c795158674752e4bc587d0a535efb72f5d
0000sir <0000sir@gmail.com>
2016-11-21 05:38:51 +0800

    init git repo

---
+++ download.rb
@@ -0,0 +1,23 @@
+#! /usr/bin/env ruby
+require 'net/http'
+require 'nokogiri'
+
+uri = URI "http://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1&mkt=en-US"
+
+xml = Net::HTTP.get(uri)
+
+#p xml
+prefix = "http://www.bing.com"
+
+doc = Nokogiri::XML(xml)
+date = doc.at_xpath('//images/image/startdate')
+img_url = doc.at_xpath('//images/image/url')
+url = prefix + img_url.content
+output_filename = date.content + ".jpg"
+ 
+#system("cd images && wget #{url} -O #{output_filename} && cd ..")
+
+pwd = `pwd`
+img_uri = "#{pwd.strip!}/images/#{output_filename}"
+
+system("gsettings set org.gnome.desktop.background picture-uri file://#{img_uri}") if File.exist?(img_uri)
